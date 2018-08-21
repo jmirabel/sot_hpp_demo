fc3be8d60be2683f7a75beba7e1d597904936225
Joseph Mirabel <jmirabel@laas.fr>
2018-08-21 13:44:46 +0200

    [talos/pickup_object] Fix supervisor.

--- talos/pickup_object/supervisor.py
+++ talos/pickup_object/supervisor.py
@@ -55,15 +55,13 @@ def makeSupervisorWithFactory (robot):
     factory.setupFrames (srdf["grippers"], srdf["handles"], robot, disabledGrippers = ["table/pose"])
     factory.addAffordance (
         Affordance ("talos/left_gripper", "box/handle1",
-            openControlType="position_torque", closeControlType="position_torque",
-            # openControlType="position", closeControlType="position",
-            refs = { "angle_open": (0,), "angle_close": (-0.5,), "torque": (-10.,) },
+            openControlType="torque", closeControlType="torque",
+            refs = { "angle_open": (0,), "angle_close": (-0.5,), "torque": (-0.05,) },
             simuParams = { "refPos": (-0.2,) }))
     factory.addAffordance (
         Affordance ("talos/left_gripper", None,
             openControlType="position", closeControlType="position",
-            # openControlType="position", closeControlType="position",
-            refs = { "angle_open": (0,), "angle_close": (-0.5,), "torque": (-10.,) },
+            refs = { "angle_open": (0,), "angle_close": (-0.5,), "torque": (-0.05,) },
             simuParams = { "refPos": (-0.2,) }))
     factory.generate ()
 
